80e5478431133bff3d33be904b0637e3b43244f3
viniveloso <viniciusveloso01@gmail.com>
2025-11-26 14:26:17 -0300

    feat: Cria entidade TaskItem com propriedades básicas

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,6 +1,9 @@
-﻿namespace TaskApi.Models
+﻿namespace TaskApi.Models;
+
+public class TaskItem
 {
-    public class TaskItem
-    {
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool Done { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
